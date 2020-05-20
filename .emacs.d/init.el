36e5347241c73419f3657610065858151ea6d12e
Gregory Jeckell <gregory.jeckell@gmail.com>
2020-05-19 23:54:48 -0700

    fix ivy minibuffer colors

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -92,10 +92,17 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (helm-projectile neotree yasnippet which-key web-mode use-package swift-mode smex smartparens rainbow-delimiters projectile prodigy popwin pallet evil counsel cider aggressive-indent ag))))
+    (helm-ag helm-projectile neotree yasnippet which-key web-mode use-package swift-mode smex smartparens rainbow-delimiters projectile prodigy popwin pallet evil counsel cider aggressive-indent ag))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+
+ ;; overwrite some ivy highlighting for mini-buffer
+ ;; use `'M-x list-faces-display` to show examples' 
+ '(ivy-minibuffer-match-face-1 ((t (:background "#404040")))) 
+ '(ivy-minibuffer-match-face-2 ((t (:background "#555555")))) 
+ '(ivy-minibuffer-match-face-3 ((t (:background "#6B396B")))) 
+ '(ivy-minibuffer-match-face-4 ((t (:background "#804480")))) 
  )
